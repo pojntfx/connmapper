30983c3ebe9d6d8a04c21d4669c791f2db91a308
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-09-03 08:04:14 +0200

    feat: Add basic CLI version for libpcap frontend

---
+++ cmd/connmapper-cli/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	dev := flag.String("dev", "eth0", "Network device to get packets from")
+
+	flag.Parse()
+
+	iface, err := net.InterfaceByName(*dev)
+	if err != nil {
+		panic(err)
+	}
+
+	handle, err := pcap.OpenLive(*dev, int32(iface.MTU), true, pcap.BlockForever)
+	if err != nil {
+		panic(err)
+	}
+	defer handle.Close()
+
+	source := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range source.Packets() {
+		if ipv4 := packet.Layer(layers.LayerTypeIPv4); ipv4 != nil {
+			layer, ok := ipv4.(*layers.IPv4)
+			if !ok {
+				continue
+			}
+
+			fmt.Printf("IPv4/%v %vB %v -> %v\n", layer.NextLayerType().String(), layer.Length, layer.SrcIP.String(), layer.DstIP.String())
+		} else if ipv6 := packet.Layer(layers.LayerTypeIPv6); ipv6 != nil {
+			layer, ok := ipv6.(*layers.IPv6)
+			if !ok {
+				continue
+			}
+
+			fmt.Printf("IPv6/%v %vB %v -> %v\n", layer.NextLayerType().String(), layer.Length, layer.SrcIP.String(), layer.DstIP.String())
+		}
+	}
+}
