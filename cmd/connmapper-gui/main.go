cc07d2c24c5877a9ad8638833ad866eaed20976e
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-09-06 14:24:37 +0200

    feat: Add embedded webserver for Parcel-built frontend

--- cmd/connmapper-gui/main.go
+++ cmd/connmapper-gui/main.go
@@ -1,29 +1,86 @@
 package main
 
 import (
-	"embed"
+	"flag"
+	"io/fs"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/phayes/freeport"
+	"github.com/pojntfx/connmapper/pkg/frontend"
+	"github.com/webview/webview"
 )
 
-//go:embed frontend/dist
-var assets embed.FS
-
 func main() {
-	app := NewApp()
+	debug := flag.Bool("verbose", false, "Enable verbose logging")
+	addr := flag.String("addr", "", "URL to open instead of the embedded webserver")
 
-	if err := wails.Run(&options.App{
-		Title:            "connmapper-gui",
-		Width:            1024,
-		Height:           768,
-		Assets:           assets,
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	}); err != nil {
-		panic(err)
+	flag.Parse()
+
+	var u *url.URL
+	if strings.TrimSpace(*addr) != "" {
+		var err error
+		u, err = url.Parse(*addr)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+		if err != nil {
+			panic(err)
+		}
+
+		port, err := freeport.GetFreePort()
+		if err != nil {
+			panic(err)
+		}
+		addr.Port = port
+
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+		root := fs.FS(frontend.UI)
+		dist, err := fs.Sub(root, "dist")
+		if err != nil {
+			panic(err)
+		}
+
+		srv := http.Server{
+			Addr:    addr.String(),
+			Handler: http.FileServer(http.FS(dist)),
+		}
+
+		go func() {
+			if err := srv.ListenAndServe(); err != nil {
+				panic(err)
+			}
+		}()
+
+		go func() {
+			<-stop
+
+			if err := srv.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		u, err = url.Parse("http://" + addr.String())
+		if err != nil {
+			panic(err)
+		}
 	}
+
+	w := webview.New(*debug)
+	defer w.Destroy()
+
+	w.SetTitle("Connmapper")
+	w.SetSize(1024, 768, webview.HintNone)
+	w.Navigate(u.String())
+
+	w.Run()
 }
