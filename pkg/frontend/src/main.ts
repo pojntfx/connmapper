206d523f6d42454a79ab89d91782be60db268bb6
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-09-07 22:49:25 +0200

    feat: Add React boilerplate

--- pkg/frontend/src/main.ts
+++
@@ -1,8 +0,0 @@
-const root = document.getElementById("root");
-
-const heading = document.createElement("h1");
-heading.innerText = "Hello, world!";
-
-root?.appendChild(heading);
-
-setInterval(() => println("Hello from JS!"), 1000);
