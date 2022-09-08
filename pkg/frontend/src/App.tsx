803bcf6d6e50978ee356d11abde86fd42bf6e788
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-09-08 17:12:19 +0200

    feat: Add basic packet fetching API

--- pkg/frontend/src/App.tsx
+++ pkg/frontend/src/App.tsx
@@ -2,5 +2,13 @@ import ReactGlobeGl from "react-globe.gl";
 import "./index.css";
 
 export default () => {
+  (async () => {
+    while (true) {
+      const packet = await getPacket();
+
+      await println(packet);
+    }
+  })();
+
   return <ReactGlobeGl />;
 };
--- pkg/frontend/src/bindings.d.ts
+++ pkg/frontend/src/bindings.d.ts
@@ -1,1 +1,19 @@
-declare function println(text: string): void;
+declare function println(val: any): Promise<void>;
+
+declare function getPacket(): Promise<Packet>;
+
+declare interface Packet {
+  layerType: string;
+  nextLayerType: string;
+  length: number;
+  srcIP: string;
+  srcCountryName: string;
+  srcCityName: string;
+  srcLongitude: number;
+  srcLatitude: number;
+  dstIP: string;
+  dstCountryName: string;
+  dstCityName: string;
+  dstLongitude: number;
+  dstLatitude: number;
+}
