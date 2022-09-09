ec21bf7af03876905040037bf3f6b5fc78794f7b
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-09-09 23:19:57 +0200

    feat: Add textures for globe and set size dynamically

--- pkg/frontend/src/App.tsx
+++ pkg/frontend/src/App.tsx
@@ -1,13 +1,27 @@
 import { useEffect } from "react";
 import ReactGlobeGl from "react-globe.gl";
+import earthTexture from "three-globe/example/img/earth-night.jpg";
+import earthElevation from "three-globe/example/img/earth-topology.png";
+import universeTexture from "three-globe/example/img/night-sky.png";
+import { useWindowSize } from "use-window-size-hook";
 import "./index.css";
 
 export default () => {
+  const { width, height } = useWindowSize();
+
   useEffect(() => {
     (window as any).handlePacket = async (packet: Packet) => {
       await println(packet);
     };
   }, []);
 
-  return <ReactGlobeGl />;
+  return (
+    <ReactGlobeGl
+      globeImageUrl={earthTexture as string}
+      bumpImageUrl={earthElevation as string}
+      backgroundImageUrl={universeTexture as string}
+      width={width}
+      height={height}
+    />
+  );
 };
