14910b63d6b82a3633b27d1486c84b7929e79c17
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-09-10 23:59:34 +0200

    feat: Add basic arc display

--- pkg/frontend/src/App.tsx
+++ pkg/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ReactGlobeGl from "react-globe.gl";
 import earthTexture from "three-globe/example/img/earth-night.jpg";
 import earthElevation from "three-globe/example/img/earth-topology.png";
@@ -6,17 +6,45 @@ import universeTexture from "three-globe/example/img/night-sky.png";
 import { useWindowSize } from "use-window-size-hook";
 import "./index.css";
 
+interface Point {
+  name: string;
+  coords: number[][];
+}
+
 export default () => {
   const { width, height } = useWindowSize();
+  const [packets, setPackets] = useState<Point[]>([]);
 
   useEffect(() => {
     (window as any).handlePacket = async (packet: Packet) => {
-      await println(packet);
+      setPackets((packets) => [
+        ...packets,
+        {
+          name: `${packet.layerType}/${packet.nextLayerType} ${packet.length}B ${packet.srcIP} (${packet.srcCountryName}, ${packet.srcCityName}, ${packet.srcLongitude}, ${packet.srcLatitude}) -> ${packet.dstIP} (${packet.dstCountryName}, ${packet.dstCityName}, ${packet.dstLongitude}, ${packet.dstLatitude})`,
+          coords: [
+            [packet.srcLongitude, packet.srcLatitude],
+            [packet.dstLongitude, packet.dstLatitude],
+          ],
+        },
+      ]);
     };
   }, []);
 
+  println(packets);
+
   return (
     <ReactGlobeGl
+      arcsData={packets}
+      arcLabel={(d: any) => (d as Point).name}
+      arcStartLat={(d: any) => (d as Point).coords[0][0]}
+      arcStartLng={(d: any) => (d as Point).coords[0][1]}
+      arcEndLat={(d: any) => (d as Point).coords[1][0]}
+      arcEndLng={(d: any) => (d as Point).coords[1][1]}
+      arcDashLength={0.4}
+      arcDashGap={0.2}
+      arcDashAnimateTime={1500}
+      arcsTransitionDuration={0}
+      arcColor={() => [`rgba(255, 255, 255, 0)`, `rgba(255, 255, 255, 255)`]}
       globeImageUrl={earthTexture as string}
       bumpImageUrl={earthElevation as string}
       backgroundImageUrl={universeTexture as string}
